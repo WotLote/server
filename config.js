f0b408643f123b192cca14a1e1d42d8f62f45a64
WotLote <jerab36rum@gmail.com>
2019-06-10 21:48:53 +0400

    '2'

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+	return {
+		port: 2000,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'st',
+	}
+}
---
+++ public/config.js
@@ -0,0 +1,7 @@
+module.exports {
+	port: 2000,
+	host: 'sql7.freemysqlhosting.net',
+	user: 'sql7294860',
+	password: '61Gyjn7u5x',
+	database: 'sql7294860',
+}
